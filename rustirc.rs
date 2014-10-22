42801014818af604c49e7da59149c4a14f5531ae
Jona Stubbe (CrystalGamma) <dev@jona-stubbe.de>
2014-10-23 00:54:51 +0200

    can now successfully join Quakenet and Freenode, will respond to PING commands

--- rustirc.rs
+++ rustirc.rs
@@ -5,95 +5,176 @@
 
 #![feature(macro_rules)]
 
-use std::io::{TcpStream, IoResult, BufferedReader};
+
+use std::io::{TcpStream, IoResult, BufferedReader, Writer, IoError, OtherIoError};
+use string_tests::StringTests;
+mod string_tests;
 
 macro_rules! assume(
     ($e:expr, $msg:expr) => (match $e { Some(ref mut e) => e, None => fail!($msg) })
 )
+macro_rules! assume_cpy(
+    ($e:expr, $msg:expr) => (match $e { Some(e) => e, None => fail!($msg) })
+)
 macro_rules! ioassume(
     ($e:expr, $msg:expr) => (match $e { Ok(e) => e, Err(msg) => fail!($msg, msg) })
 )
+macro_rules! tryopt( ($e:expr, $default:expr) => (match $e {Some(e)=>e, None=>return $default}))
 
-
-pub struct Connection {
-	write: Box<TcpStream>,
-	read: Box<BufferedReader<TcpStream>>,
-	names: Option<Box<Iterator<String> + 'static>>,
-	nick: Option<String>,
-	user_name: String,
-	real_name: String
+trait IrcLine{
+	fn decode_irc_event<'a>(&'a self) -> Option<IrcEvent<'a>>;
 }
-
-pub trait StringTests {
-	fn is_valid_nick(&self) -> bool;
-	fn no_newline(&self) -> bool;
+struct IrcEvent<'a> {
+	prefix: &'a str,
+	cmd: &'a str,
+	args: Vec<&'a str>
 }
-
-impl<'a> StringTests for &'a str {
-	fn is_valid_nick(&self) -> bool {
-		println!("Nickname test for: {}", self);
-		let mut iter = self.bytes();
-		let b = iter.next();
-		match  b {
-		Some(48u8...57u8) | None => {return false;} // 0-9
-		_ => {}
-		}
-
-		for c in self.bytes() {
-			match c {
-			65u8...90u8 | 97u8...122u8 | 48u8 ...57u8 => continue, // A-Z, a-z, 0-9
-			_ => return false
-			}
-		}
-		true
-	}
-	
-	fn no_newline(&self) -> bool {
-		println!("No newline test for: {}", self);
-		!self.chars().any(|c| c=='\r' || c=='\n')
+impl<'t> IrcLine for &'t str {
+	fn decode_irc_event<'a>(&'a self) -> Option<IrcEvent<'a>> {
+		let line = self.trim_right_chars(|c: char| c == '\r' || c == '\n');
+		let bytes = line.as_bytes();
+		let hasprefix = bytes[0] == 58; // ':'
+		let mut parts = line.splitn(if hasprefix {2} else {1}, |c: char| c == ':');
+		if hasprefix { parts.next(); }
+		let mut segments = tryopt!(parts.next(), None).split(|c: char| c == ' ');
+		let prefix = if hasprefix {tryopt!(segments.next(), None)} else {""};
+		let cmd = tryopt!(segments.next(), None);
+		let mut args: Vec<&str> = segments.filter(|st: &&str|st.len()>0).collect();
+		match parts.next() {
+			Some(x)=>args.push_all([x]),
+			None=>{}
+		};
+		Some(IrcEvent {prefix: prefix, cmd: cmd, args:args})
 	}
 }
+/*impl IrcLine for String {
+	fn decode_irc_event<'a>(&'a self) -> Option<IrcEvent<'a>> {
+		self.as_slice().decode_irc_event()
+	}
+}*/
 
-impl StringTests for String {
-	fn is_valid_nick(&self) -> bool {
-		self.as_slice().is_valid_nick()
-	}
-	fn no_newline(&self) -> bool {
-		self.as_slice().no_newline()
-	}
+struct IrcReader<T> {
+	read: BufferedReader<T>
 }
 
+impl<T: Reader> Iterator<String> for IrcReader<T> {
+	fn next(&mut self) -> Option<String> {
+		let st = ioassume!(match self.read.lines().next() {Some(x)=>x, None=>return None}, "could not read event: {}");
+		Some(st)
+	}
+}
+impl<T: Reader> IrcReader<T> {
+	pub fn new(read: T) -> IrcReader<T> { IrcReader {read: BufferedReader::new(read)} }
+}
 
-impl Connection {
-	fn nick_message(&mut self) -> IoResult<()> {
-		self.nick = assume!(self.names, "nick_message called after successful nickname selection").next();
-		let nick = assume!(self.nick,"nick name generator did not generate enough nicks");
-		assert!(nick.is_valid_nick());
-		let out = format!("NICK {}\r\nUSER {} - - 8 :{}\r\n", nick, self.user_name, self.real_name);
+trait CloseWrite {
+	fn close_write(&mut self) -> IoResult<()>;
+}
+
+impl CloseWrite for TcpStream {
+	fn close_write(&mut self) -> IoResult<()> { self.close_write() }
+}
+
+struct IrcWriter<T> {
+	write: T
+}
+
+impl<T: Writer + CloseWrite> IrcWriter<T> {
+	pub fn new(write: T) -> IrcWriter<T> { IrcWriter{write: write} }
+//	pub fn get_inner(&self) -> &T { &self.write }
+	fn nick_message(&mut self, nick: &str, user_name: &str, real_name: &str) -> IoResult<()> {
+		let out = format!("NICK {}\r\nUSER {} 8 * :{}\r\n", nick, user_name, real_name);
 		print!("{}",out);
 		try!(self.write.write_str(out.as_slice()));
 		Ok(())
 	}
-	pub fn connect(conn: TcpStream, names: Box<Iterator<String> + 'static>, user_name: String, real_name: String) -> IoResult<Connection> {
+	fn quit(&mut self) -> IoResult<()> {
+		try!(self.write.write_str("QUIT\r\n"));
+		self.write.close_write()
+	}
+	fn join(&mut self, channel: &str) -> IoResult<()> {
+		assert!(channel.no_newline());
+		self.write.write_str(format!("JOIN :{}\r\n", channel).as_slice())
+	}
+	fn pong(&mut self, data: &str) -> IoResult<()> {
+		assert!(data.no_newline());
+		self.write.write_str(format!("PONG :{}\r\n", data).as_slice())
+	}
+}
+
+pub struct Connection {
+	read: IrcReader<TcpStream>,
+	write: IrcWriter<TcpStream>,
+	nick: String,
+	nick_status: NickStatus,
+	user_name: String,
+	real_name: String
+}
+
+enum NickStatus {
+	Registering(Box<Iterator<String> + 'static>),
+	Accepted
+}
+
+
+impl Connection {
+	pub fn connect<T: Iterator<String> + 'static>(conn: TcpStream, mut names: T, user_name: String, real_name: String) -> IoResult<Connection> {
 		assert!(user_name.is_valid_nick() && real_name.no_newline());
 		let mut irc = Connection {
-			read: box BufferedReader::new(conn.clone()),
-			write: box conn,
-			names: Some(names),
-			nick: None,
+			read: IrcReader::new(conn.clone()),
+			write: IrcWriter::new(conn),
+			nick: match names.next() {Some(x)=>x, None=>fail!("nick name generator did not generate enough nicks")},
+			nick_status: Registering(box names),
 			user_name: user_name,
 			real_name: real_name};
-		try!(irc.nick_message());
+		assert!(irc.nick.is_valid_nick());
+		try!(irc.write.nick_message(irc.nick.as_slice(), irc.user_name.as_slice(), irc.real_name.as_slice()));
 		Ok(irc)
 	}
 	
 	pub fn eventloop(&mut self) -> IoResult<()> {
-		loop {
-			let line = match self.read.lines().next() {Some(x)=>x,None=>break};
-			match self.names {
-				Some(_) => {try!(self.nick_message());},
-				_ =>{}};
-			println!("{}", line);
+		//let mut tries = 5u;
+		for event in self.read {
+			/*match self.nick_status {
+				Registering(_) => {
+					try!(self.write.nick_message(self.nick.as_slice(), self.user_name.as_slice(), self.real_name.as_slice()));
+				},
+				Accepted =>{}};*/
+			let slice = event.as_slice();
+			let ev = tryopt!(slice.decode_irc_event(), Err(IoError{kind: OtherIoError, desc: "malformed IRC event received", detail: None}));
+			println!("{}  prefix:{}\n  command: {}\n  args:{}", event, ev.prefix, ev.cmd, ev.args);
+			match ev.cmd {
+			"PRIVMSG" => {
+				if ev.args.len() != 2 {
+					return Err(IoError{kind: OtherIoError, desc: "malformed PRIVMSG command received", detail: None})
+				}
+				let text = match ev.args.last() {None => unreachable!(), Some(x) => x};
+				println!("message recieved: {}", text);
+				if *text == "!kill" {
+					try!(self.write.quit());
+				}
+			},
+			"PING" => {
+				if ev.args.len() != 1 {
+					return Err(IoError{kind: OtherIoError, desc: "malformed PING command received", detail: None})
+				}
+				try!(self.write.pong(ev.args[0]));
+			},
+			"001" => {
+				self.nick_status = Accepted;
+				println!("Server accepted nickname {}", self.nick);
+				try!(self.write.join("#Deathmic"));
+			},
+			"433" | "436" => {
+				self.nick = match self.nick_status {
+					Registering(ref mut iter) => assume_cpy!(iter.next(),"nick name generator did not generate enough nicks"),
+					Accepted => return Err(IoError{kind: OtherIoError, desc: "unexpected 433 ERR_NICKNAMEINUSE or 436 ERR_NICKCOLLISION received", detail: None})};
+				assert!(self.nick.is_valid_nick());
+				println!("Rejected, trying nickname {}", self.nick);
+				try!(self.write.nick_message(self.nick.as_slice(), self.user_name.as_slice(), self.real_name.as_slice()));
+			},
+			_ => {}
+			}
 		}
 		Ok(())
 	}
@@ -117,9 +198,9 @@ impl Iterator<String> for NickGenerator {
 
 pub fn main() {
 	let mut conn = ioassume!(Connection::connect(
-		ioassume!(TcpStream::connect("chat.freenode.net", 6667), "TCP Connection failed: {}"),
-		box NickGenerator {basename: "CrystalGBot", attempt:0},
+		ioassume!(TcpStream::connect("irc.quakenet.org", 6667), "TCP Connection failed: {}"),
+		NickGenerator {basename: "CrystalGBot", attempt:0},
 		"CrystalGBot".to_string(),
 		"CrystalGamma experimental chat bot implemented in Rust".to_string()), "IRC connection failed: {}");
-	conn.eventloop();
+	ioassume!(conn.eventloop(),"main loop failed: {}");
 }
